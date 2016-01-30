6b37a31712726be6b80858cd4d0e88f999a516eb
Chengsong <shoumatu1996@gmail.com>
2016-01-30 14:24:10 -0800

    Edited the structure, run the demon to start the server

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 var express = require('express');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var places = require('./routes/places');
 var atob = require('./routes/atob');
+var tsp = require('./routes/tsp');
+
 var app = express();
 
-app.get('/places', function (req, res) {
-  res.send(places.getTopPlaces());
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.get('/places', places);
+
+app.get('/atob', atob);
+
+app.get('/tsp', tsp);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.get('/atob', function (req, res) {
-  res.send(atob.navigate());
-});
+// error handlers
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+
+module.exports = app;
