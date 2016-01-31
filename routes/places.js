9a42e1bb1b47dac58ec2253115c8e129f49d1f0a
Oscar Tu <Oscar Tu>
2016-01-30 17:32:32 -0800

    Implement places API

--- routes/places.js
+++ routes/places.js
@@ -1,3 +1,16 @@
+var places = require('../services/places');
+
 module.exports = function(req, res) {
-	res.send('working');
+	if (req.query.origin == null) {
+		res.status = 400;
+		return res.send('Please enter a location');
+	}
+	places(req.query.origin)
+	.then(function(placeList) {
+		res.send(placeList);
+	})
+	.catch(function(err) {
+		res.status = 500;
+		res.send(err);
+	});
 }
--- services/places.js
+++ services/places.js
@@ -1,3 +1,15 @@
-module.exports = function(req, res) {
-	res.send('working');
+//key: Yh5B7Is6s91ey3vZXvdY5IOWsMldQXcq
+
+var request = require('request-promise');
+
+module.exports = function(origin, startDate, endDate, callback) {
+//	var start = startDate;
+//	var end = endDate;
+	var city = origin;
+//	if (startDate != undefined && endDate != undefined) {
+//		return request.get('http://terminal2.expedia.com/x/activities/search?location=London&startDate=' + startDate + '&endDate=' + endDate + '&apikey=Yh5B7Is6s91ey3vZXvdY5IOWsMldQXcq');		
+//	} else {
+	//http://terminal2.expedia.com/x/activites/details?activityId=169503&apikey=Yh5B7Is6s91ey3vZXvdY5IOWsMldQXcq
+	return request.get('http://terminal2.expedia.com/x/activities/search?location=' + city + '&apikey=Yh5B7Is6s91ey3vZXvdY5IOWsMldQXcq');
+//	}
 }
