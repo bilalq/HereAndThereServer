03df4e2d3526bdb2a4444f480f0e58dd3a5f213e
ZebraLord <mali9299@gmail.com>
2016-01-31 12:33:17 -0800

    atob working wtih ranking. currently just does walk, bus, and bike.

--- services/atob.js
+++ services/atob.js
@@ -5,15 +5,27 @@ var walk = require("./transportation/walk");
 var bike = require("./transportation/bike");
 var Promise = require('bluebird');
 
+var AVG_SALARY_PER_SEC = 0.00693629807
+
 var compareTransportation = function(a, b) {
-  return a;
+  costA = a.time * AVG_SALARY_PER_SEC + a.cost;
+  costB = b.time * AVG_SALARY_PER_SEC + b.cost;
+
+  if (costA === costB) {
+    return 0;
+  }
+  if (costA < costB) {
+    return -1;
+  }
+
+  return 1;
 }
 
 module.exports = function(origin, destination) {
   directionsPromises = [walk(origin, destination),
                         bus(origin, destination),
-                        bike(origin, destination),
-                        car(origin, destination)]
+                        bike(origin, destination)]//,
+                        //car(origin, destination)]
 
   return Promise.all(directionsPromises).then(function(directions) {
     return directions.sort(compareTransportation);
